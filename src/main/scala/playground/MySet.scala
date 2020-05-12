7bce88e3ea75a68590d17fc5af9d74f780afba42
Bartosz Gajda <bg@bartoszgajda.com>
2020-05-12 16:27:12 +0100

    Potentially Infinite Set.

--- src/main/scala/playground/MySet.scala
+++ src/main/scala/playground/MySet.scala
@@ -14,6 +14,17 @@ trait MySet[A] extends (A => Boolean) {
   def flatMap[B](f: A => MySet[B]): MySet[B]
   def filter(predicate: A => Boolean): MySet[A]
   def foreach(f: A => Unit): Unit
+
+  /**
+    * - removing an element
+    * - intersection with another set
+    * - difference with another set
+    */
+  def -(elem: A): MySet[A]
+  def &(anotherSet: MySet[A]): MySet[A]
+  def --(anotherSet: MySet[A]): MySet[A]
+
+  def unary_! : MySet[A]
 }
 
 class EmptySet[A] extends MySet[A] {
@@ -25,6 +36,45 @@ class EmptySet[A] extends MySet[A] {
   def flatMap[B](f: A => MySet[B]): MySet[B] = new EmptySet[B]
   def filter(predicate: A => Boolean): MySet[A] = this
   def foreach(f: A => Unit): Unit = ()
+
+  def -(elem: A): MySet[A] = this
+  def &(anotherSet: MySet[A]): MySet[A] = this
+  def --(anotherSet: MySet[A]): MySet[A] = this
+
+  def unary_! : MySet[A] = new PropertyBasedSet[A](x => true)
+}
+
+// Describes all elements of type A which satisfy a property
+// { x in A | property(a) }
+class PropertyBasedSet[A](property: A => Boolean) extends MySet[A] {
+  def contains(elem: A): Boolean = property(elem)
+
+  def +(elem: A): MySet[A] =
+    new PropertyBasedSet[A](x => property(x) || x == elem)
+
+  def ++(anotherSet: MySet[A]): MySet[A] =
+    new PropertyBasedSet[A](x => property(x) || anotherSet(x))
+
+  def map[B](f: A => B): MySet[B] = politelyFail
+
+  def flatMap[B](f: A => MySet[B]): MySet[B] = politelyFail
+
+  def filter(predicate: A => Boolean): MySet[A] =
+    new PropertyBasedSet[A](x => property(x) && predicate(x))
+
+  def foreach(f: A => Unit): Unit = politelyFail
+
+  def -(elem: A): MySet[A] = filter(x => x != elem)
+
+  def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+
+  def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+
+  def unary_! : MySet[A] = new PropertyBasedSet[A](x => !property(x))
+
+  def politelyFail =
+    throw new IllegalArgumentException("really deep rabbit hole")
+
 }
 
 class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
@@ -52,6 +102,18 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
     f(head)
     tail foreach f
   }
+
+  def -(elem: A): MySet[A] =
+    if (head == elem) tail
+    else tail - elem + head
+
+  def &(anotherSet: MySet[A]): MySet[A] =
+    filter(anotherSet)
+
+  def --(anotherSet: MySet[A]): MySet[A] =
+    filter(!anotherSet)
+
+  def unary_! : MySet[A] = new PropertyBasedSet[A](x => !this.contains(x))
 }
 
 object MySet {
@@ -68,4 +130,14 @@ object MySet {
 object MySetPlayground extends App {
   val s = MySet(1, 2, 3, 4)
   s + 5 ++ MySet(-1, -2) + 3 foreach println
+
+  val negative = !s
+  println(negative(2))
+  println(negative(5))
+
+  val negativeEven = negative.filter(_ % 2 == 0)
+  println(negativeEven(5))
+
+  val negativeEven5 = negativeEven + 5
+  println(negativeEven5(5))
 }
