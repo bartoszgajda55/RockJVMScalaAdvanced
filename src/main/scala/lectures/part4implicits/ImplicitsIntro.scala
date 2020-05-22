053805edfe7a74c04d8863cca35bed055348e36d
Bartosz Gajda <bg@bartoszgajda.com>
2020-05-22 18:42:35 +0100

    Implicits Intro.

---
+++ src/main/scala/lectures/part4implicits/ImplicitsIntro.scala
@@ -0,0 +1,25 @@
+package lectures.part4implicits
+
+object ImplicitsIntro extends App {
+  val pair = "Daniel" -> "555"
+  val intPair = 1 -> 2
+
+  case class Person(name: String) {
+    def greet = s"My name is $name"
+  }
+
+  implicit def fromStringToPerson(str: String): Person = Person(str)
+
+  println("Peter".greet)
+
+  // class A {
+  //   def greet: Int = 2
+  // }
+  // implicit def fromStringToA(str: String): A = new A
+
+  // implicit parameters
+  def increment(x: Int)(implicit amount: Int) = x + amount
+  implicit val defaultAmount = 100
+
+  increment(42)
+}
