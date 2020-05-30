c06f245cf7596ebd577e9c99ec254f73a8412931
Bartosz Gajda <bg@bartoszgajda.com>
2020-05-30 13:43:13 +0200

    Higher Kinded Types.

---
+++ src/main/scala/lectures/part5typesystem/HigherKindedTypes.scala
@@ -0,0 +1,64 @@
+package lectures.part5typesystem
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object HigherKindedTypes extends App {
+
+  trait AHigherKindedType[F[_]]
+
+  trait MyList[T] {
+    def flatMap[B](f: T => B): MyList[B]
+  }
+
+  trait MyOption[T] {
+    def flatMap[B](f: T => B): MyOption[B]
+  }
+
+  trait MyFuture[T] {
+    def flatMap[B](f: T => B): MyFuture[B]
+  }
+
+  // combine/multiply
+  def multiply[A, B](listA: List[A], listB: List[B]): List[(A, B)] =
+    for {
+      a <- listA
+      b <- listB
+    } yield (a, b)
+
+  def multiply[A, B](listA: Option[A], listB: Option[B]): Option[(A, B)] =
+    for {
+      a <- listA
+      b <- listB
+    } yield (a, b)
+
+  def multiply[A, B](listA: Future[A], listB: Future[B]): Future[(A, B)] =
+    for {
+      a <- listA
+      b <- listB
+    } yield (a, b)
+
+  trait Monad[F[_], A] {
+    def flatMap[B](f: A => F[B]): F[B]
+    def map[B](f: A => B): F[B]
+  }
+
+  class MonadList extends Monad[List, Int] {
+    def flatMap[B](f: Int => List[B]): List[B] = ???
+    def map[B](f: Int => B): List[B] = ???
+  }
+
+  def multiply[F[_], A, B](ma: Monad[F, A], mb: Monad[F, B]): F[(A, B)] =
+    for {
+      a <- ma
+      b <- mb
+    } yield (a, b)
+
+  val monadList = new MonadList
+  monadList.flatMap(x => List(x, x + 1))
+  // Monad[List, Int] => List[Int]
+  monadList.map(_ * 2)
+  // Monad[List, Int] => List[Int]
+
+  multiply(new MonadList, new MonadList)
+}
